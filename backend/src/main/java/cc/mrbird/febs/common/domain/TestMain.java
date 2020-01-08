b0b5ef643f5e8a242af9efdd33497046cfa4eccc
wangjianfeng <remixknighx@sina.cn>
2020-01-08 23:36:55 +0800

    启动正常

---
+++ backend/src/main/java/cc/mrbird/febs/common/domain/TestMain.java
@@ -0,0 +1,11 @@
+package cc.mrbird.febs.common.domain;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println(Long.valueOf(RandomStringUtils.randomNumeric(10)));
+    }
+
+}
